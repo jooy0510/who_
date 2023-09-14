4a8056cc4af61214cad6e6c5aab2330b7d7c1827
ChaedongIm <chaedong.im.dev@gmail.com>
2023-09-14 17:09:42 +0900

    Chore: OG 태그 추가, 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,15 @@ import { PropsWithChildren } from 'react';
 export const metadata: Metadata = {
   title: 'WHO',
   description: ' 응원단 WHO',
+  openGraph: {
+    title: 'WHO',
+    description: '수원하이텍 고등학교 응원단 WHO',
+    url: 'https://who-alpha.vercel.app/',
+    siteName: 'WHO',
+
+    locale: 'ko_KR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
