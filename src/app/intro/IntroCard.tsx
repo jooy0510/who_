121ea04e0b8681651a0b50d70bb69ba489d9bd4e
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-19 23:48:03 +0900

    Update IntroCard.tsx

--- src/app/intro/IntroCard.tsx
+++ src/app/intro/IntroCard.tsx
@@ -23,7 +23,7 @@ export default function IntroCard() {
           removeWrapper
           alt="Relaxing app background"
           className="z-0 w-full h-full object-cover"
-          src="who_/2.jpeg"
+          src="/who_/2.jpeg"
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
