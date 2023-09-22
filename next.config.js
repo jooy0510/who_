9fb6dab494ebd908d136a319279b9947a803f8be
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-22 21:32:05 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
         port: '',
         pathname: '/**',
