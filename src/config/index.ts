8929b941cb859e5dd3540b62a8da0ea2b2e99d22
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-22 21:36:31 +0900

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
+export const NOTION_DATABASE_ID_2 = process.env.NOTION_DATABASE_ID_2;
 export const NOTION_TOKEN = process.env.NOTION_TOKEN;
 export const NOTION_CALENDAR_ID = process.env.NOTION_CALENDAR_ID;
 
