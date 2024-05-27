56bac31142ca3ef5294d1910df8f8f173ce94853
loud <78677354+jooy0510@users.noreply.github.com>
2024-05-27 21:15:11 +0900

    전화번호, 소개글 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,12 +6,12 @@ export const NOTION_CALENDAR_ID = process.env.NOTION_CALENDAR_ID;
 export const GMAIL_ID = process.env.GMAIL_ID;
 export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
 
-export const INFO_PHONE = '010-8456-0549';
+export const INFO_PHONE = '010-8498-0159';
 export const INFO_INSTA = 'cheerleader.who';
 export const INFO_ADDRESS = '경기도 수원시 영통구 영통동 청명북로 56';
 
 export const INTRO_MESSAGE = [
   'We are Hero Of us',
   '안녕하세요, 수원하이텍고등학교 치어리딩 동아리 WHO 응원단입니다!',
-  '2000년부터 이어진 치어리딩 동아리로 현재까지도 활발하게 활동하고 있습니다:)',
+  '2000년부터 이어진, 2023 마이스터예술제 대상을 수상한 동아리입니다:)',
 ];
